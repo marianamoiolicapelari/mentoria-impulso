8e91f45194601f674dace8d6904b85a1d8dd90a4
marianamoiolicapelari <marianamoioli@gmail.com>
2022-06-23 20:44:42 -0300

    Update aula18-06.js

--- aula18-06.js
+++ aula18-06.js
@@ -13,26 +13,35 @@
 let clientes = []
 
 
-function exibirClientes(){
+function exibirClientes() {
   console.log(clientes)
-} 
+}
 
 
-function adicionarCliente(name,surename,email,cpf,date){
-  let cliente = {name, surename:surename ,email:email, cpf:cpf, date:date}
+function adicionarCliente(name, surename, email, cpf, date) {
+  let cliente = { name, surename, email, cpf, date }
   clientes.push(cliente)
 }
 
-function excluirCliente(){
-  clientes.splice(0,1)
+
+
+adicionarCliente('Maria', 'Silva', 'mariasilva@gmail.com', 22222222222, '11/11/2011')
+adicionarCliente('Fa', 'Souza', 'joaosouza@gmail.com', 55555555555, '11/01/1987')
+adicionarCliente('João', 'Souza', 'joaosouza@gmail.com', 55555555555, '11/01/1987')
+adicionarCliente('Pedro', 'Lima', 'pedrolima@gmail.com', 99999999999, '30/12/1990')
+adicionarCliente('Luiza', 'Silva', 'luizasilva@gmail.com', 77777777777, '15/01/1990')
+
+
+function excluirCliente(arr, prop, value) {
+  return clientes.filter(function(i) {return i[prop] !== value;})
 }
 
+let excluirCliente2 = excluirCliente(clientes, "name", "Fa")
 
-adicionarCliente('Maria','Silva','mariasilva@gmail.com',22222222222,'11/11/2011')
-adicionarCliente('Fa','Souza', 'joaosouza@gmail.com',55555555555,'11/01/1987')
-adicionarCliente('João','Souza', 'joaosouza@gmail.com',55555555555,'11/01/1987')
-adicionarCliente('Pedro','Lima', 'pedrolima@gmail.com',99999999999,'30/12/1990')
-adicionarCliente('Luiza','Silva','luizasilva@gmail.com',77777777777, '15/01/1990')
+console.log(excluirCliente2)
 
-excluirCliente()
-exibirClientes()
+// console.log("Usuário não encontrado!")
+
+
+
+
