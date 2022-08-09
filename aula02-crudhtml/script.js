181519655dd3f369c9c84ed288fdf79ebc97a0ad
Mariana M.C <marianamoioli@gmail.com>
2022-08-09 10:19:14 -0300

    Added alt

--- aula02-crudhtml/script.js
+++ aula02-crudhtml/script.js
@@ -1,11 +1,18 @@
 const button = document.getElementById('btn-form-input')
 const form = document.getElementById('add-form-users')
 
+let users = []
+
 button.addEventListener( 'click', () => {
     
     const formData = new FormData(form)
     const { name, surname, email, phone } = Object.fromEntries(formData)
-    console.log(name)
+    const user = { name, surname, email, phone }
+    users.push(user)
+
+    console.log(user)
 })
+
+
 
 
