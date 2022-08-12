e2b249fb05b4b12348e4acad1f35f59f5f878315
Mariana M.C <marianamoioli@gmail.com>
2022-08-12 17:38:52 -0300

    added exercises

---
+++ CRUD/js/script.js
@@ -0,0 +1,2 @@
+const formClient = document.getElementById('form-cad-client')
+const btnAddClient = document.getElementById('btn-cad-client')
--- aula02-crudhtml/script.js
+++ aula02-crudhtml/script.js
@@ -1,18 +1,43 @@
 const button = document.getElementById('btn-form-input')
 const form = document.getElementById('add-form-users')
+const table = document.getElementById('table-users')
 
 let users = []
 
-button.addEventListener( 'click', () => {
-    
+const addUser = () => {
+
+    users.forEach((user) => {
+        let tr = document.createElement('tr')
+        let td = document.createElement('td')
+
+        td.innerText = user.name
+        tr.appendChild(td)
+
+        td = document.createElement('td')
+        td.innerText = user.surname
+        tr.appendChild(td)
+
+        td = document.createElement('td')
+        td.innerText = user.phone
+        tr.appendChild(td)
+
+        td = document.createElement('td')
+        td.innerText = user.email
+        tr.appendChild(td)
+
+        table = document.querySelector('tbody').appendChild(tr)
+
+
+    })
+}
+
+button.addEventListener('click', () => {
+
     const formData = new FormData(form)
-    const { name, surname, email, phone } = Object.fromEntries(formData)
-    const user = { name, surname, email, phone }
+    const { name, surname, phone, email } = Object.fromEntries(formData)
+    const user = { name, surname, phone, email }
     users.push(user)
 
-    console.log(user)
+    addUser()
+
 })
-
-
-
-
