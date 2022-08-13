7ae298e30f88cccd3ba85d409229138ec39d026f
Mariana M.C <marianamoioli@gmail.com>
2022-08-13 09:46:01 -0300

    added alt

--- CRUD/js/script.js
+++ CRUD/js/script.js
@@ -1,2 +1,43 @@
 const formClient = document.getElementById('form-cad-client')
 const btnAddClient = document.getElementById('btn-cad-client')
+const tableUsers = document.getElementById('table-users')
+
+const users = []
+
+const addUser = () => {
+    const tbody = document.createElement('tbody')
+    tableUsers.querySelector('tbody').remove()
+    tableUsers.appendChild(tbody)
+
+    users.forEach((user) => {
+        let tr = document.createElement('tr')
+        let td = document.createElement('td')
+
+        td.innerText = user.name
+        tr.appendChild(td)
+
+        td = document.createElement('td')
+        td.innerText = user.surname
+        tr.appendChild(td)
+
+        td = document.createElement('td')
+        td.innerText = user.phone
+        tr.appendChild(td)
+
+        td = document.createElement('td')
+        td.innerText = user.email
+        tr.appendChild(td)
+
+        tableUsers.querySelector('tbody').appendChild(tr)
+    })
+}
+
+btnAddClient.addEventListener('click', () => {
+    const userData = new FormData(formClient)
+    const { name, surname, phone, email } = Object.fromEntries(userData)
+    const user = { name, surname, phone, email }
+    users.push(user)
+
+    addUser()
+})
+
