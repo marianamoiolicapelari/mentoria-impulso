c280e61e72568aa1e69d7ff9e64eb158adceaf5f
Mah Moioli Capelari <100173586+marianamoiolicapelari@users.noreply.github.com>
2022-08-15 16:21:54 -0300

    Update script.js

--- CRUD/js/script.js
+++ CRUD/js/script.js
@@ -1,43 +1,67 @@
-const formClient = document.getElementById('form-cad-client')
-const btnAddClient = document.getElementById('btn-cad-client')
-const tableUsers = document.getElementById('table-users')
+const btnAddUser = document.getElementById("btn-form-add-user")
+const formAddUser = document.getElementById("form-add-user")
+const tableUsers = document.getElementById("table-users")
 
-const users = []
+let users = []
 
-const addUser = () => {
-    const tbody = document.createElement('tbody')
-    tableUsers.querySelector('tbody').remove()
-    tableUsers.appendChild(tbody)
-
-    users.forEach((user) => {
-        let tr = document.createElement('tr')
-        let td = document.createElement('td')
-
-        td.innerText = user.name
-        tr.appendChild(td)
-
-        td = document.createElement('td')
-        td.innerText = user.surname
-        tr.appendChild(td)
-
-        td = document.createElement('td')
-        td.innerText = user.phone
-        tr.appendChild(td)
-
-        td = document.createElement('td')
-        td.innerText = user.email
-        tr.appendChild(td)
-
-        tableUsers.querySelector('tbody').appendChild(tr)
-    })
+const deleteUser = (id) => {
+  users = users.filter((user) => user.id !== id)
+  createTable()
 }
 
-btnAddClient.addEventListener('click', () => {
-    const userData = new FormData(formClient)
-    const { name, surname, phone, email } = Object.fromEntries(userData)
-    const user = { name, surname, phone, email }
-    users.push(user)
+const addUser = () => {
+  const formData = new FormData(formAddUser)
+  const { name, surname, email } = Object.fromEntries(formData)
 
-    addUser()
+  const id = Math.floor(Math.random() * 9999)
+
+  const user = {id, name, surname, email}
+
+  users.push(user)
+  createTable()
+}
+
+btnAddUser.addEventListener('click', () => {
+  addUser()
 })
 
+
+const createTable = () => {
+  const tbody = document.createElement('tbody')
+  tableUsers.querySelector('tbody').remove()
+  tableUsers.appendChild(tbody)
+
+  users.forEach((user) => {
+    let tr = document.createElement('tr')
+    let td  = document.createElement('td')
+
+    let btnDelete = document.createElement('button')
+    btnDelete.setAttribute('class', 'btn-success')
+    btnDelete.setAttribute('onclick', `deleteUser(${user.id})`)
+    btnDelete.innerText = 'Deletar'
+
+    td.innerText = user.id
+    tr.appendChild(td)
+
+    td = document.createElement('td')
+    td.innerText = user.name
+    tr.appendChild(td)
+
+    td = document.createElement('td')
+    td.innerText = user.surname
+    tr.appendChild(td)
+
+    td = document.createElement('td')
+    td.innerText = user.email
+    tr.appendChild(td)
+
+    td = document.createElement('td')
+    td.appendChild(btnDelete)
+    tr.appendChild(td)
+
+    tbody.appendChild(tr)
+
+    tableUsers.appendChild(tbody)
+
+  })
+}
