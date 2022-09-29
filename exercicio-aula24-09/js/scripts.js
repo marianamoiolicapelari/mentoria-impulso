85561865afe4a6623e29e35be0a094c2018ddc85
Mariana M.C <marianamoioli@gmail.com>
2022-09-29 09:01:34 -0300

    adicionado alterações em exercicio24-09

--- exercicio-aula24-09/js/scripts.js
+++ exercicio-aula24-09/js/scripts.js
@@ -4,16 +4,32 @@ const tableAddUser = document.getElementById('table-add-user')
 
 const url = 'https://jsonplaceholder.typicode.com/users'
 
+const responseToJson = (response) => response.json()
+
+const onSuccess = (response) => {
+    createTablePosts(response)
+}
+
 fetch(url)
-    .then((response) => response.json())
-    .then((response) => {
-        createTablePosts(response)
-    })
+    .then(responseToJson)
+    .then(onSuccess)
+
+
+const clearTable = () => {
+    const tbody = document.createElement('tbody')
+    tableAddUser.querySelector('tbody').remove()
+    tableAddUser.appendChild(tbody)
+}
 
 const createTablePosts = (users) => {
     users.forEach((user) => {
         const tr = document.createElement('tr')
         let td = document.createElement('td')
+
+        let btnDelete = document.createElement('button')
+        btnDelete.setAttribute('class', 'btn-delete')
+        btnDelete.setAttribute('onclick', `deleteUser(${user.email})`)
+        btnDelete.innerText = 'Deletar'
 
         td.innerText = user.name
         tr.appendChild(td)
@@ -30,9 +46,46 @@ const createTablePosts = (users) => {
         td.innerText = user.email
         tr.appendChild(td)
 
+        td = document.createElement('td')
+        td.appendChild(btnDelete)
+        tr.appendChild(td)
+
         tableAddUser.querySelector('tbody').appendChild(tr)
 
     })
+}
+
+const onCreateUserSuccessToJson = (response) => response.json()
+
+const onCreateUserSuccess = (response) => {
+    const tr = document.createElement('tr')
+    let td = document.createElement('td')
+
+    td.innerText = response.name
+    tr.appendChild(td)
+
+    td = document.createElement('td')
+    td.innerText = response.username
+    tr.appendChild(td)
+
+    td = document.createElement('td')
+    td.innerText = response.phone
+    tr.appendChild(td)
+
+    td = document.createElement('td')
+    td.innerText = response.email
+    tr.appendChild(td)
+
+    let btnDelete = document.createElement('button')
+    btnDelete.setAttribute('class', 'btn-delete')
+    btnDelete.setAttribute('onclick', `deleteUser(${response.email})`)
+    btnDelete.innerText = 'Deletar'
+
+    td = document.createElement('td')
+    td.appendChild(btnDelete)
+    tr.appendChild(td)
+
+    tableAddUser.querySelector('tbody').appendChild(tr)
 }
 
 const addUser = () => {
@@ -46,31 +99,17 @@ const addUser = () => {
             'Content-type': 'application/json; charset=UTF-8'
         }
     })
-        .then((response) => response.json())
-        .then((response) => {
-
-            const tr = document.createElement('tr')
-            let td = document.createElement('td')
-
-            td.innerText = user.name
-            tr.appendChild(td)
-
-            td = document.createElement('td')
-            td.innerText = user.username
-            tr.appendChild(td)
-
-            td = document.createElement('td')
-            td.innerText = user.phone
-            tr.appendChild(td)
-
-            td = document.createElement('td')
-            td.innerText = user.email
-            tr.appendChild(td)
-
-            tableAddUser.querySelector('tbody').appendChild(tr)
-
-        })
+        .then(onCreateUserSuccessToJson)
+        .then(onCreateUserSuccess)
 }
 
 btnAddUser.addEventListener('click', addUser)
 
+
+const deleteUser = (email) => {
+
+    fetch(`https://jsonplaceholder.typicode.com/user/${email}`, {
+        method: 'DELETE',
+    })
+
+}
